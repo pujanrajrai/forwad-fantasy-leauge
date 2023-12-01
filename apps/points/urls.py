b32d26cfc8f5993af8de85cda3edb18bc75b5723
pratik213 <dahalp30@gmail.com>
2023-12-01 22:40:23 +0545

    done with crud

--- apps/points/urls.py
+++ apps/points/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('create/', views.MatchDayCreateView.as_view(), name="matchday_create"),
     path('<int:pk>/update/', views.MatchDayUpdateView.as_view(), name="matchday_update"),
     
+    path('playerpoint/list/', views.PlayerPointsListView.as_view(), name="playerpoint_list"),
+    path('playerpoint/create/', views.PlayerPointsCreateView.as_view(), name="playerpoint_create"),
+    path('playerpoint/<int:pk>/update/', views.PlayerPointUpdateView.as_view(), name="playerpoint_update"),
+    
+    path('teamresult/list/', views.TeamResultListView.as_view(), name="teamresult_list"),
+    path('teamresult/create/', views.TeamResultCreateView.as_view(), name="teamresult_create"),
+    path('teamresult/<int:pk>/update/', views.TeamResultUpdateView.as_view(), name="teamresult_update"),
+
 ]
--- apps/points/views.py
+++ apps/points/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from points.models.matchday import MatchDay
 from points.models.points import PlayerPoints
+from points.models.teamresult import TeamResult
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
-from .forms import MatchDayCreateForm,PlayerPointsCreateForm
+from .forms import MatchDayCreateForm,PlayerPointsCreateForm,TeamResultCreateForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 # Create your views here.
@@ -51,8 +52,51 @@ class PlayerPointsListView(ListView):
 class PlayerPointsCreateView(CreateView):
     model = PlayerPoints
     form_class = PlayerPointsCreateForm
-    template_name = 'points/PlayerPoints/create.html'  # Update with your actual template name
-    success_url = reverse_lazy('points:playerpoints_list')  # Update with your actual success URL
+    template_name = 'points/playerpoint/create.html'  # Update with your actual template name
+    success_url = reverse_lazy('points:playerpoint_list')  # Update with your actual success URL
+
+    def form_valid(self, form):
+        # Optionally, you can perform additional actions when the form is valid
+        return super().form_valid(form)
+
+class PlayerPointUpdateView(UpdateView):
+    model = PlayerPoints
+    form_class = PlayerPointsCreateForm  # Use the same form as in your CreateView
+    template_name = 'points/playerpoint/update.html'  # Update with your actual template name
+    success_url = reverse_lazy('points:playerpoint_list')  # Update with your actual success URL
+
+    def form_valid(self, form):
+        # Optionally, you can perform additional actions when the form is valid
+        return super().form_valid(form)
+
+
+
+# Team Result
+class TeamResultListView(ListView):
+    model = TeamResult
+    template_name = 'points/teamresult/list.html'
+    context_object_name = 'teamresult_list'  # Specify the variable name in the template
+
+    def get_queryset(self):
+        # Customize the queryset if needed
+        return TeamResult.objects.all()
+
+
+class TeamResultCreateView(CreateView):
+    model = TeamResult
+    form_class = TeamResultCreateForm
+    template_name = 'points/teamresult/create.html'  # Update with your actual template name
+    success_url = reverse_lazy('points:teamresult_list')  # Update with your actual success URL
+
+    def form_valid(self, form):
+        # Optionally, you can perform additional actions when the form is valid
+        return super().form_valid(form)
+
+class TeamResultUpdateView(UpdateView):
+    model = TeamResult
+    form_class = TeamResultCreateForm  # Use the same form as in your CreateView
+    template_name = 'points/teamresult/update.html'  # Update with your actual template name
+    success_url = reverse_lazy('points:teamresult_list')  # Update with your actual success URL
 
     def form_valid(self, form):
         # Optionally, you can perform additional actions when the form is valid
